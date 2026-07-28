eafe30dceb0575732fec7aefeefa0097d3c0ad5a
Perez01180 <perezgabrielbenjamin@gmail.com>
2026-07-28 16:48:40 -0300

    actualizamos las consultas del frontend

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,42 +1,19 @@
+const API_BASE = "http://localhost:8080/api/v2";
+
 export async function getUser(id) {
-    const response = await fetch("http://localhost:8080/user/" + id);
+    const response = await fetch(API_BASE + "/users/" + id);
     const user = await response.json();
     return user;
 }
 
-function saveLocalUser(){
-    localStorage.setItem("usuario-logueado", JSON.stringify({
-        isLogged : false,
-        userId : null
-    }))
-}
-
-export function logOutUser(){
-    localStorage.clear("usuario-logueado");
-    window.location = "/index.html"
-}
-
-export function getLocalUser(){
-    const userData = localStorage.getItem("usuario-logueado");
-    if(!userData){
-        saveLocalUser();
-        return {
-            isLogged : false,
-            userId : null
-        }
-    }
-    const localUser = JSON.parse(userData);
-    return localUser;
-}
-
 export async function getUsers(){
-    const response = await fetch("http://localhost:8080/users/");
+    const response = await fetch(API_BASE + "/users/");
     const data = await response.json();
     return data.users;
 }
 
 export async function updateUserRole(id, role){
-    const response = await fetch("http://localhost:8080/user/" + id + "/role", {
+    const response = await fetch(API_BASE + "/users/" + id + "/role", {
         method: "put",
         headers : {"content-type" : "application/json"},
         body : JSON.stringify({ role })
@@ -46,9 +23,19 @@ export async function updateUserRole(id, role){
 }
 
 export async function deleteUser(id){
-    const response = await fetch("http://localhost:8080/user/" + id, {
+    const response = await fetch(API_BASE + "/users/" + id, {
         method :  "delete"       
     });
     const data = await response.json();
     return data; 
+}
+
+export async function login(username, password){
+    const response = await fetch (API_BASE + "/auth/login",{
+        method : "post",
+        headers : {"content-type" : "application/json"},
+        body : JSON.stringify({ username, password })
+    });
+    const data = await response.json();
+    return data;
 }

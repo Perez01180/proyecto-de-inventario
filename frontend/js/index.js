c0d78465efb09e106e9dd0cdc3390cbfe3b10254
Perez01180 <perezgabrielbenjamin@gmail.com>
2026-08-04 17:04:31 -0300

    Recuperamos el token y actualizamos el login e index.html

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,16 +1,17 @@
-import { getUser, getLocalUser } from "./utils.js";
+import { getMyUser, getToken } from "./utils.js";
 
 
 async function main() {
-    const localUser = getLocalUser();
-    if (localUser.isLogged === true) {
-        const dataUser = await getUser(localUser.userId);
-
+    const token = getToken();
+    if (token) {
+        const dataUser = await getMyUser(token);
+       
         const mainIndex = document.getElementById("main-index");
         const p = document.createElement("p");
         p.innerText = "bienvenido " + dataUser.username + " Tu rol es " + dataUser.role;
 
         mainIndex.appendChild(p);
+       
     }
 }
 main();
--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -31,10 +31,41 @@ export async function deleteUser(id){
 }
 
 export async function login(username, password){
-    const response = await fetch (API_BASE + "/auth/login",{
+    const response = await fetch(API_BASE + "/auth/login",{
         method : "post",
         headers : {"content-type" : "application/json"},
         body : JSON.stringify({ username, password })
+    });
+    const data = await response.json();
+    return data;
+}
+
+export async function register(name, lastname, dni, username, password){
+    const response = await fetch(API_BASE + "/auth/register",{
+        method : "post",
+        headers : {"content-type" : "application/json"},
+        body : JSON.stringify({ name, lastname, dni, username, password })
+    });
+    const data = await response.json();
+    return data;
+}
+
+export function saveToken(token){
+    localStorage.setItem("userToken", token);
+}
+
+export function getToken(){
+    const token = localStorage.getItem("userToken");
+    return token;
+}
+
+export async function getMyUser(token){
+    const response = await fetch(API_BASE + "/users/me", {
+        method : "get",
+        headers : { 
+            "Authorization" : "Bearer " + token,
+            "Content-Type" : "application/json"
+        }
     });
     const data = await response.json();
     return data;
